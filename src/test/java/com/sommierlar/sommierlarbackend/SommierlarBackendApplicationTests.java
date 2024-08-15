b5c977c765681ea98946ce44a393b817f3a42f8a
EmilianoChavezDev <eemirandachavez@gmail.com>
2024-08-15 10:54:23 -0400

    subiendo archivos iniciales

---
+++ src/test/java/com/sommierlar/sommierlarbackend/SommierlarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sommierlar.sommierlarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SommierlarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
