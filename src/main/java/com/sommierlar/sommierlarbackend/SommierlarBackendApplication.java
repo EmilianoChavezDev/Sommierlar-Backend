490b9cb7adf4afa0e41449c4f753646bfdc12538
EmilianoChavezDev <eemirandachavez@gmail.com>
2024-08-15 10:54:23 -0400

    subiendo archivos iniciales

---
+++ src/main/java/com/sommierlar/sommierlarbackend/SommierlarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sommierlar.sommierlarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SommierlarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SommierlarBackendApplication.class, args);
+    }
+
+}
